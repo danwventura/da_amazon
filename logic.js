35a7113b644cc9a986e170702bd258bd5b968fb0
danwventura <danwventura@gmail.com>
2024-11-21 14:28:54 -0500

    Dynamically loading html

--- logic.js
+++ logic.js
@@ -1,1 +1,32 @@
-console.log("script loaded");
+console.log("logic loaded")
+
+function loadPage(page) {
+    fetch(page + '.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        });
+
+ }
+
+// Initial page load (Home page)
+loadPage('home');
+
+//Dynamic loading of page
+document.querySelector('ul').addEventListener('click', (event) => {
+    console.log("clicked")
+    if (event.target.tagName.toLowerCase() === 'a') {
+        event.preventDefault(); 
+        loadPage(event.target.getAttribute('href')); 
+    }
+});
+
+//Navigate to checkout
+document.querySelector('body').addEventListener('click', (event) => {
+    console.log("clicked 2")
+    if (event.target.id === 'checkout') {
+        event.preventDefault(); 
+        loadPage(event.target.id); 
+    }
+});
+
